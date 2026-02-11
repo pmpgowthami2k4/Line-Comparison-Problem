596ef8710a79d944c3c756ae20d0b946d697d737
Gowthami <pmpgowthami2k4@gmail.com>
2026-02-11 12:04:41 +0530

    Added Calculate length of a line.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            // Creating a line with two points
+            Line line = new Line(1, 2, 4, 6);
+
+            // Calculating length
+            double length = line.CalculateLength();
+
+            Console.WriteLine("Length of the Line is: " + length);
         }
     }
 }
